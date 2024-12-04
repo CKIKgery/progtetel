eb4b28d61e6166392645563e5ce399d56437e8f1
Apro <farkas.gergo@ckik.loc>
2024-12-04 11:11:56 +0100

    ezaz csanigeci

--- progtetelcig/Program.cs
+++ progtetelcig/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 
 namespace progtetelcig
@@ -9,6 +10,30 @@ namespace progtetelcig
         {
             int[] x = { 1, 2, -5, 12, 5, 3, };
             //1. Párosak kivalogatasa
+
+            int n = x.Length;
+            int i;
+            int[] kimenet = new int[100];
+            int db = 0;
+            for (i=0; i < n; i++)
+            {
+                if (x[i] % 2 == 0)
+                {
+                    kimenet[db] = x[i];
+                    db++;
+                }
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(kimenet[i]+" ");
+            }
+
+
+
+
+
+
             //2.Maximum
         }
     }
